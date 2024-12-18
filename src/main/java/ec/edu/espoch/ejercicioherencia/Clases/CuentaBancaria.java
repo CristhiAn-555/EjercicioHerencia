0282ba3f5cb2d150cbc28b0560b961716e48a87c
CristhiAn-555 <cristhianvalencia999@gmail.com>
2024-12-18 14:44:01 -0500

    hola

--- src/main/java/ec/edu/espoch/ejercicioherencia/Clases/CuentaBancaria.java
+++ src/main/java/ec/edu/espoch/ejercicioherencia/Clases/CuentaBancaria.java
@@ -10,10 +10,52 @@ package ec.edu.espoch.ejercicioherencia.Clases;
  */
 public class CuentaBancaria {
     
-     protected float saldo;
+    protected float saldo;
     protected int numeroConsignaciones = 0;
     protected int numeroRetiros = 0;
     protected float tasaAnual;
     protected float comisionMensual = 0;
+
+    public CuentaBancaria(float saldo, float tasaAnual) {
+        this.saldo = saldo;
+        this.tasaAnual = tasaAnual;
+    }
     
+    public void consignar(float cantidad) {
+        if (cantidad > 0) {
+            saldo += cantidad;
+            numeroConsignaciones++;
+        }
+    }
+
+    public void retirar(float cantidad) {
+        if (cantidad > 0 && cantidad <= saldo) {
+            saldo -= cantidad;
+            numeroRetiros++;
+        } else {
+            System.out.println("Saldo insuficiente para retirar.");
+        }
+    }
+
+    public void calcularInteresMensual() {
+        float tasaMensual = tasaAnual / 12;
+        float interesMensual = saldo * tasaMensual;
+        saldo += interesMensual;
+    }
+
+    public void extractoMensual() {
+        saldo -= comisionMensual;
+        calcularInteresMensual();
+    }
+
+    public void imprimir() {
+        int transaccionesTotales = numeroConsignaciones + numeroRetiros;
+        System.out.println("Saldo: " + saldo);
+        System.out.println("Número de transacciones: " + transaccionesTotales);
+        System.out.println("Tasa anual: " + tasaAnual);
+        System.out.println("Comisión mensual: " + comisionMensual);
+    }
 }
+    
+    
+    
