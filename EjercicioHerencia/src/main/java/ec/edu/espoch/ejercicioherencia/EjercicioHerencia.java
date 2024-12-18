2ebda07943e36f5fdd58504a53d7d60c0dd8b8fa
haro124 <haroldcarrono1@gmail.com>
2024-12-18 14:55:41 -0500

    subir

--- EjercicioHerencia/src/main/java/ec/edu/espoch/ejercicioherencia/EjercicioHerencia.java
+++ EjercicioHerencia/src/main/java/ec/edu/espoch/ejercicioherencia/EjercicioHerencia.java
@@ -11,6 +11,28 @@ package ec.edu.espoch.ejercicioherencia;
 public class EjercicioHerencia {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //  Cuenta de Ahorros
+        System.out.println("Cuenta de Ahorros:");
+        CuentaAhorros cuentaAhorros = new CuentaAhorros(15000, 0.05f);
+        cuentaAhorros.consignar(5000);
+        cuentaAhorros.retirar(2000);
+        cuentaAhorros.retirar(3000);
+        cuentaAhorros.retirar(2000);
+        cuentaAhorros.retirar(5000); 
+        cuentaAhorros.extractoMensual();
+        cuentaAhorros.imprimir();
+
+        //  Cuenta Corriente
+        System.out.println("Cuenta Corriente:");
+        CuentaCorriente cuentaCorriente = new CuentaCorriente(10000, 0.04f);
+        cuentaCorriente.consignar(2000);
+        cuentaCorriente.retirar(5000);
+        cuentaCorriente.retirar(8000); 
+        cuentaCorriente.consignar(3000); 
+        cuentaCorriente.extractoMensual();
+        cuentaCorriente.imprimir();
     }
 }
+    
+
